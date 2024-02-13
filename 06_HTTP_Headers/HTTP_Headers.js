c0bff6020ebea255d393c77978575615e18ad0d9
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-02-13 18:57:10 +0500

    Chapter 6: Headers.

---
+++ 06_HTTP_Headers/HTTP_Headers.js
@@ -0,0 +1,48 @@
+/*
+    What are HTTP headers?
+    An HTTP header allows clients and servers to pass additional information with each request or response.
+    Header are just case-insensitive key-value pairs that pass additional metadata about the request or response.
+
+    HTTP requests from a web browser carry with them many headers, including but not limited to:
+        The type of slient (e.g. Google Chrome).
+        The Operating System (e.g. Linux).
+        THe preferred language (e.g. US English).
+
+    As a Developer, we can also define custom headers in each request.
+*/
+
+/*
+    Headers API.
+    The Headers API allows us to perform various actions on our request and response headers such as retrieving settings, and removing them.
+    We can access the headers object through the Request.headers and Response.headers properties.
+*/
+
+/*
+    We can use Location in Headers.
+    Like for example, for different countries, the price will show based on the location.
+*/
+
+/*
+    Browser's Developer Tools.
+    Modern web browsers offer developers a powerful set of developer tools.
+    The Developer Tools are a front=end web developer's best friend!
+
+    For Example, using the dev tools you can:
+        View the web page's JavaScript console output.
+        Inspect the page's HTML, CSS, and JavaScript Code.
+        View network requests and responses along with their headers.
+
+    The method for accessing dev tools varies from browser to browser.
+*/
+
+/*
+    The Network Tab.
+    The Network tab monitors your browser's network activity and records all of the requests and responses the browser is making, including how long each of those request and responses takes to fully process.
+*/
+
+/*
+    Why are headers useful?
+    Headers are useful fo rseveral reasons from design to security, but most often headers are used as metadata or data about the request.
+
+    We can use headers for authenticaion.
+*/
