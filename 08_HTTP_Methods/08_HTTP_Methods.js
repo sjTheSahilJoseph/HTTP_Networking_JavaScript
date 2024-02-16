c1732baf00bf1ec93281c6e832b8fcea510861c2
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-02-16 20:46:54 +0500

    rest tomorrow

---
+++ 08_HTTP_Methods/08_HTTP_Methods.js
@@ -0,0 +1,14 @@
+// HTTP Methods - GET.
+// HTTP defines a set of methods that we use every time we make a request.
+
+// The GET method.
+// The GET method us used to 'get' a representation of a specified resource.
+// You are not taking the data away from the server, but rather getting a representation,
+// or copy, of the resource in its current state.
+// A get request is considered a safe method to call multiple times because it doesn't alter the state of the server.
+
+// Making a GET request using the Fetch API.
+// The fetch() method accepts an optional init object parameter as its second argument that we can use to define things like:
+// method: The HTTP method of the request, like GET.
+// headers: The header to send.
+// mode: Used for security.
