da58bfc7f2f2f4f4225e5ffacae419f92ad437a1
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-02-20 18:24:11 +0500

    Chapter 10: HTTPS

---
+++ 10_HTTPS/10_HTTPS.js
@@ -0,0 +1,35 @@
+/*
+    HTTPS
+    Hypertext Transfer Protocol Secure or HTTPS is an extension of the HTTP protocol.
+    HTTPS secures the data transfer between client and server by encrypting all of the communication.
+
+    HTTPS allows a client to safely share sensitive information with the server through an HTTP request, such as credit card information, passwords, or bank account numbers.
+*/
+
+/*
+    Basically, there are kind of 2 keys, the server generates.
+        1. Public Key
+        2. Private Key
+    Public key -> Encryption
+    Private Key -> Decryption
+
+    But then the 3rd key is generated which can do both encryption/decryption.
+
+    This way, the data is being sefe.
+*/
+
+/*
+    Security and Encryption
+    HTTPS requires that the client use SSL or TLS to protect requests and traffic by encrypting the information in the request.
+    HTTPS is just HTTP with extra security.
+
+    HTTPS keeps your messages private, but not your identity
+    While HTTPS encrypts what you are saying. It doesn't necassarily protect who you are.
+    Tools like VPNs are needed for remaining anonymous online.
+
+    HTTPS ensures that you're talking to the right person (or server)
+    In addition to encrypting the information within a request, HTTPS uses digital signatures to prove that you're communicating with the server that you think you are.
+    If a hacker were to intercept an HTTPS request by tapping into a network cable, they wouldn't be able to successfully pretend they are you bank's web server.
+*/
+
+
